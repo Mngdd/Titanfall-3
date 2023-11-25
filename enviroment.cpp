926d77c67072209b85a6732161a3046ae8b7e7eb
ichinosekei <egorlop2005@mail.ru>
2023-11-25 12:04:06 +0300

    egor
    write class player with some questions

--- enviroment.cpp
+++ enviroment.cpp
@@ -1,5 +1,44 @@
 //
 // Created by khris on 22.11.2023.
 //
-
+#include <bits/stdc++.h>
 #include "enviroment.h"
+
+using namespace std;
+// что я должен делать когда хп меньше 0
+class Player
+{private:
+    string name;
+    double health;
+    double damage;
+    int x, y;
+    
+public:
+
+    Player(string namePlayer, double health_point, int xcord, int ycord){
+        name = namePlayer;
+        health = health_point;
+        x = xcord;
+        y = ycord;}
+
+    double GetHealth(){
+        return health;
+    }
+
+    int GetCords(){
+        return x, y;
+    }
+
+    void SetDamage(double damagevalue){
+        damage = damagevalue;
+        health -= damage;
+        if (health <= 0){GetHealth();}}
+};
+
+class Func_trace
+{
+
+};
+
+
+
