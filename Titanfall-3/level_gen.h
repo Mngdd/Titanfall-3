43684891265ed1beba0d696f24a217f443021e26
gavkosmig1 <iumanov.vm@phystech.edu>
2023-12-01 18:36:51 +0300

    Update level_gen.h

--- Titanfall-3/level_gen.h
+++ Titanfall-3/level_gen.h
@@ -9,16 +9,17 @@
 // Затем генерируютяс препятствия между игроками
 // Затем добавляются случайные препятствия
 
-// Но надо всю генерацию переработать, т.к. без прямых прострелов это почти неиграбельно и скучно и невозможно попасть и вообще ужас
+// Но надо всю генерацию переработать, т.к. без прямых прострелов это почти неиграбельно и скучно и невозможно попасть и
+// вообще ужас
 
 // Вариант генерации:
 // Сначала генерируются препятствия, потом игроки
 // n - количество игроков
-// 1. Сначала появляется n кругов большого радиуса в почти случайном месте таких, что вероятнее будет ближе к краю карты.
-// (координата будет браться в квадрате или другой степени больше 1, нужна возможность отрисовки)
+// 1. Сначала появляется n кругов большого радиуса в почти случайном месте таких, что вероятнее будет ближе к краю
+// карты. (координата будет браться в квадрате или другой степени больше 1, нужна возможность отрисовки)
 // 2. Затем появляется n кругов среднего радиуса так, что что вероятнее всего будут ближе к середине карты
 // (кординаты будут браться в степени меньше 1, требуется возможность отрисовки)
-// 3. Затем в совершенно случайных местах появляется n маленьких кругов 
+// 3. Затем в совершенно случайных местах появляется n маленьких кругов
 // 4. Затем генерируются игроки на малом расстоянии друг от друга
 
 // Карта будет скорее всего 1200:720
@@ -30,22 +31,24 @@
 //
 // При смерти игрок появляется на достаточном расстоянии от противников
 
-#include <cmath>
-#include <Graph_lib/Point.h>
-#include <Graph_lib/Graph.h>
-#include <Graph_lib/Simple_window.h>
 #include "std_lib_facilities.h"
+#include <Graph_lib/Graph.h>
+#include <Graph_lib/Point.h>
+#include <Graph_lib/Simple_window.h>
 using namespace Graph_lib;
 
-void HugeObsSpawn();     // adds obstacles to HugeObstacles
-void MediumObsSpawn();   // adds obstacles to MediumObstacles
-void SmallObsSpawn();    // adds obstacles to SmallObstacles
-void PlayersSpawn();     // adds players to Players
+void HugeObsSpawn ();    // adds obstacles to HugeObstacles
+void MediumObsSpawn ();  // adds obstacles to MediumObstacles
+void SmallObsSpawn ();   // adds obstacles to SmallObstacles
+void PlayersSpawn ();    // adds players to Players
+
+void Generate(); // Generates everything
 
 extern vector<Circle*> HugeObstacles;
 extern vector<Circle*> MediumObstacles;
 extern vector<Circle*> SmallObstacles;
 
+extern vector<Circle*> Obstacles;
 extern vector<Circle*> Players;
 
-#endif //TITANFALL_3_LEVEL_GEN_H
+#endif  // TITANFALL_3_LEVEL_GEN_H
