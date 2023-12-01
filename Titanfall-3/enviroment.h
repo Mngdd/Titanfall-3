9884887f9f812c1488ff43879d4d20149398cf00
Mngdd <74361463+Mngdd@users.noreply.github.com>
2023-12-01 19:14:45 +0300

    Update enviroment.h
    нужно внутри дефов делать, иначе неоч будет

--- Titanfall-3/enviroment.h
+++ Titanfall-3/enviroment.h
@@ -1,12 +1,11 @@
 //
 // Created by khris on 22.11.2023.
 //
+#ifndef TITANFALL_3_ENVIROMENT_H
+#define TITANFALL_3_ENVIROMENT_H
 
 int GetCords();
 double GetHealth();
 void SetDamage(double damagevalue);
-
-#ifndef TITANFALL_3_ENVIROMENT_H
-#define TITANFALL_3_ENVIROMENT_H
 
 #endif //TITANFALL_3_ENVIROMENT_H
