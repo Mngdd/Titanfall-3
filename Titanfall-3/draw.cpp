0dbb0ec7b1daf5831d56a7f0264b40bdeaabfa47
MarkovAlexandr314 <markov314@bk.ru>
2023-12-04 16:44:48 +0300

    draw

--- Titanfall-3/draw.cpp
+++ Titanfall-3/draw.cpp
@@ -18,41 +18,59 @@ void drawing()
 {
 
     Generate(); // generation obstacles and players
-    std::cout << Obstacles.empty();
+    ObstaclesRespawn();
+    std::cout << Obstacles.size();
+    std::cout << Players.size();
 
     Game_window win{Point(100, 100), FieldLength, FieldWidth, "Window"};
 
-    // for (size_t i{0}; i < q.size(); ++i)
-    // {
-    // Point *m = new Point(100, 100);
-    // Players.push_back(m);
-    // }
-
     win.wait_for_button();
 
-    // for (size_t i{0}; i < Obstacles.size(); ++i) // draw obstacles
-    // {
-    //     Circle *c = new Circle{Obstacles[i]->center, Obstacles[i]->radius};
-    //     c->set_fill_color(HugeObsColor);
-    //     show_obs.push_back(c);
-    // };
+    for (size_t i{0}; i < Players.size(); ++i)
+    {
+        Player *m = new Player{*pl_name[0], 3, Players[i]->x, Players[i]->y};
+        pl.push_back(m);
+    }
 
-    // for (auto i : show_obs)
-    // {
-    //     win.attach(*i);
-    // }
+    for (size_t i{0}; i < Obstacles.size(); ++i) // draw obstacles
+    {
+        Circle *c = new Circle{Obstacles[i]->center, Obstacles[i]->radius};
+        c->set_fill_color(HugeObsColor);
+        show_obs.push_back(c);
+    };
 
-    // for (size_t i{0}; i < pl.size(); ++i) // draw obstacles
-    // {
-    //     Circle *c = new Circle{Point(pl[i]->GetCords().first, pl[i]->GetCords().second), PlayerRad};
-    //     c->set_fill_color(PlayerColor);
-    //     show_pl.push_back(c);
-    // };
+    for (auto i : show_obs)
+    {
+        win.attach(*i);
+    }
 
-    // for (auto i : show_pl)
-    // {
-    //     win.attach(*i);
-    // }
+    for (size_t i{0}; i < pl.size(); ++i) // draw pl
+    {
+        Circle *c = new Circle{Point(pl[i]->GetCords().first, pl[i]->GetCords().second), PlayerRad};
+        c->set_fill_color(PlayerColor);
+        show_pl.push_back(c);
+    };
+
+    for (auto i : show_pl)
+    {
+        win.attach(*i);
+    }
+
+    for (size_t i{0}; i < pl.size(); ++i) // draw nm
+    {
+        Text *c{nullptr};
+        if (pl[i]->GetCords().second + PlayerRad >= FieldWidth)
+            c = new Text{Point(pl[i]->GetCords().first, pl[i]->GetCords().second + PlayerRad), pl[i]->GetName()};
+        else
+            c = new Text{Point(pl[i]->GetCords().first, pl[i]->GetCords().second - PlayerRad), pl[i]->GetName()};
+        c->set_font_size(20);
+        show_nm.push_back(c);
+    };
+
+    for (auto i : show_nm)
+    {
+        win.attach(*i);
+    }
 
     win.wait_for_button();
 
