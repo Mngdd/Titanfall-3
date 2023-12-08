7f18d9f17e0f14df0b36aabe15a2c958ea615d98
MarkovAlexandr314 <markov314@bk.ru>
2023-12-08 13:51:54 +0300

    settings norm

--- Titanfall-3/settings.h
+++ Titanfall-3/settings.h
@@ -1,31 +1,43 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
+#include <Graph_lib/Graph.h>
+
 const int FieldLength = 1200; // Длина поля
 const int FieldWidth = 720;   // Ширина поля
 
-// ИГРА
+// РАСКРАСКА
 
-const int WhiteObsRad = 5; // радиус вырезов препятсвий
+inline Color HugeObsColor = Color::black;
+inline Color MediumObsColor = Color::black;
+inline Color SmallObsColor = Color::black;
+
+inline bool RainbowPlayers = true; // Случайный цвет игроков
+inline Color PlayerColor = Color::green;
+// Color Team1PlayerColor = Color::blue;
+// Color Team2PlayerColor = Color::red;
 
 // ГЕНЕРАЦИЯ
 
 // УБРАТЬ const ПЕРЕД ТЕМИ НАСТРОЙКАМИ, ЧТО МОЖНО БУДЕТ МЕНЯТЬ В МЕНЮШКЕ!!
 
-inline int NumOfPlayers{2}; // ТРЕБУЕТСЯ ЗАДАВАТЬ
+inline int NumOfPlayers = 2; // ТРЕБУЕТСЯ ЗАДАВАТЬ
 
-const int HugeObsPower = 2;     // Степень ухода больших препятствий от центра
-const int MediumObsPower = 0.5; // Степень близости средних препятствий к центру
+const int PlayerRad = 10; // Радиус игрока
+
+const double HugeObsPower = 2;     // Степень ухода больших препятствий от центра
+const double MediumObsPower = 0.5; // Степень ухода средних препятствий от центра
+const double SmallObsPower = 1;    // Степень ухода маленьких препятствий от центра
 
 const int SpawnBetwMinDist = 70; // Минимальное расстояние между игроками (соло режим)
 const int SpawnWallMinDist = 30; // Минимальное расстояние от игрока до стены (больше радиуса игрока)
 const int SpawnObsMinDist = 5;   // Минимальное расстояние от игрока до препятствия
 
-const int PlayerRad = 15; // Радиус игрока
+const int SpawnObsWallMinDist = 3; // Минимальное расстояние от препятствия до стены
 
-const int NumOfHugeObs = 5; // Количество препятствий каждого вида
-const int NumOfMediumObs = 5;
-const int NumOfSmallObs = 5;
+const int NumOfHugeObs = 1; // Количество препятствий каждого вида
+const int NumOfMediumObs = 1;
+const int NumOfSmallObs = 1;
 
 const int HugeObsMaxRad = 100; // Радиусы препятствий разных размеров
 const int HugeObsMinRad = 65;
@@ -33,5 +45,8 @@ const int MediumObsMaxRad = 65;
 const int MediumObsMinRad = 30;
 const int SmallObsMaxRad = 30;
 const int SmallObsMinRad = 15;
+
+const int PlayerSpawnTries = 100;     // Количество попыток заспавнить игрока в норм месте
+const int ObstacleReSpawnTries = 100; // Количество попыток зареспавнить препятствие в норм месте
 
 #endif
