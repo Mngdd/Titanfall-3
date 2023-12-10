3a6e5cc72a0a370ec052b7fb967b806da2759f60
gavkosmig1 <144454449+gavkosmig1@users.noreply.github.com>
2023-12-10 18:16:34 +0300

    Update Point.h

--- lib/Graph_lib/Point.h
+++ lib/Graph_lib/Point.h
@@ -35,8 +35,6 @@ inline Point operator- (Point a, Point b) { return a -= b; }
 inline bool operator== (Point a, Point b) { return a.x == b.x && a.y == b.y; }
 
 inline bool operator!= (Point a, Point b) { return !(a == b); }
-
-inline double dist (Point a, Point b) { return pow(pow(a.x - b.x, 2) + pow(a.y - b.y, 2), 0.5);}
 }  // namespace Graph_lib
 
 #endif  // POINT_GUARD
