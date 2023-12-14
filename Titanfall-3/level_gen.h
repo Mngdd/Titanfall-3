5700a3a3022d00d5d964a3781888034f2de2c6d2
gavkosmig1 <iumanov.vm@phystech.edu>
2023-12-14 21:43:59 +0300

    ТЕПЕРЬ РАНДОМ РАБОТАЕТ

--- Titanfall-3/level_gen.h
+++ Titanfall-3/level_gen.h
@@ -8,10 +8,13 @@
 #include <vector>
 
 // random tech
-struct PRNG {
+struct PRNG
+{
     std::mt19937 engine;
 };
-struct GenerationSettings {
+
+struct GenerationSettings
+{
     // Количество игроков для которых надо сгенерировать
     const int NumOfPlayers;
     // Радиус игрока
@@ -22,22 +25,22 @@ struct GenerationSettings {
     const double MediumObsPower = 0.5;
     // Степень ухода маленьких препятствий от центра
     const double SmallObsPower = 1;
-    
+
     // Минимальное расстояние между игроками (мультиплеер)
     const int SpawnBetwMinDist = 70;
     // Минимальное расстояние от игрока до стены (больше радиуса игрока)
     const int SpawnWallMinDist = 30;
     // Минимальное расстояние от игрока до препятствия
     const int SpawnObsMinDist = 5;
-    
+
     // Минимальное расстояние от препятствия до стены
     const int SpawnObsWallMinDist = 3;
-    
+
     // Количество препятствий каждого вида
     const int NumOfHugeObs = 5;
     const int NumOfMediumObs = 5;
     const int NumOfSmallObs = 5;
-    
+
     // Радиусы препятствий разных размеров
     const int HugeObsMaxRad = 100;
     const int HugeObsMinRad = 65;
@@ -45,34 +48,37 @@ struct GenerationSettings {
     const int MediumObsMinRad = 30;
     const int SmallObsMaxRad = 30;
     const int SmallObsMinRad = 15;
-    
+
     // Количество попыток заспавнить игрока в норм месте
     const int PlayerSpawnTries = 500;
     // Количество попыток зареспавнить препятствие в норм месте
     const int ObstacleReSpawnTries = 100;
 };
 
-double dist(Graph_lib::Point a, Graph_lib::Point b);// returns dist between two points
+double dist (Graph_lib::Point a, Graph_lib::Point b);  // returns dist between two points
 
-void initGenerator(PRNG &generator);                                   // generates seed for random
-unsigned random(PRNG &generator, unsigned minValue, unsigned maxValue);// gives random ints
+void initGenerator (PRNG& generator);                                     // generates seed for random
+unsigned random (PRNG& generator, unsigned minValue, unsigned maxValue);  // gives random ints
 
-Graph_lib::Point NotsoRandomPoint(double Power);// Point "Power" far away from the center
+Graph_lib::Point NotsoRandomPoint (double Power);  // Point "Power" far away from the center
 
 // obstacles generation
-void HugeObsSpawn(std::vector<Obstacle> &Obs_vec);  // adds obstacles to HugeObstacles
-void MediumObsSpawn(std::vector<Obstacle> &Obs_vec);// adds obstacles to MediumObstacles
-void SmallObsSpawn(std::vector<Obstacle> &Obs_vec); // adds obstacles to SmallObstacles
+void HugeObsSpawn (std::vector<Obstacle>& Obs_vec);    // adds obstacles to HugeObstacles
+void MediumObsSpawn (std::vector<Obstacle>& Obs_vec);  // adds obstacles to MediumObstacles
+void SmallObsSpawn (std::vector<Obstacle>& Obs_vec);   // adds obstacles to SmallObstacles
 
-bool ObsDistPlayerCheck(const Obstacle &Obs, const std::vector<Player> &players_vec);// obstacle not overlapping player
-void ObstaclesRespawn(std::vector<Obstacle> &Obs_vec);                               // regenerates obstacles with existing players
+bool ObsDistPlayerCheck (const Obstacle& Obs,
+                         const std::vector<Player>& players_vec);  // obstacle not overlapping player
+void ObstaclesRespawn (std::vector<Obstacle>& Obs_vec);            // regenerates obstacles with existing players
 
 // players generation
-bool PlayerDistObsCheck(const Graph_lib::Point &Player_pos, const std::vector<Obstacle> &Obs_vec);      // Player not overlapping Obstacles
-bool PlayerDistPlayersCheck(const Graph_lib::Point &Player_pos, const std::vector<Player> &players_vec);// Player not overlapping Players
+bool PlayerDistObsCheck (const Graph_lib::Point& Player_pos,
+                         const std::vector<Obstacle>& Obs_vec);  // Player not overlapping Obstacles
+bool PlayerDistPlayersCheck (const Graph_lib::Point& Player_pos,
+                             const std::vector<Player>& players_vec);  // Player not overlapping Players
 
-void GenerateObstacles(std::vector<Obstacle> &obstacles);// Generates everything at the start of the game
-void GeneratePlayer(Player &player_, const std::vector<Obstacle> &obstacles,
-                    const std::vector<Player> &players_vec);// Generates everything at the start of the game
+void GenerateObstacles (std::vector<Obstacle>& obstacles);  // Generates everything at the start of the game
+void GeneratePlayer (Player& player_, const std::vector<Obstacle>& obstacles,
+                     const std::vector<Player>& players_vec);  // Generates everything at the start of the game
 
-#endif// TITANFALL_3_LEVEL_GEN_H
+#endif  // TITANFALL_3_LEVEL_GEN_H
